29be767e58069194a641df3e05bd3d309ca708ae
WilmarParedesRomero <u25209113@utp.edu.pe>
2025-08-13 21:33:40 -0500

    commit01

---
+++ EjemploO1.java
@@ -0,0 +1,12 @@
+//Ejemplos de notacion Big O
+// O(1) - Acceso a un elemento por índice
+public class EjemploO1 {
+    public static int obtenerPrimerElemento(int[] lista) {
+        return lista[0]; // Siempre una sola operación
+    }
+
+    public static void main(String[] args) {
+        int[] numeros = {5, 2, 9, 1, 7};
+        System.out.println(obtenerPrimerElemento(numeros));
+    }
+}
