73f8701a19acc569d3649eb4aebb8902bd07b3ad
WilmarParedesRomero <U25209113@utp.edu.pe>
2025-10-12 23:40:13 -0500

    Update GestorEstudiantes.java

--- GestorEstudiantes.java
+++ GestorEstudiantes.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Scanner; 
           
 public class GestorEstudiantes {
     public static void main(String[] args) { 
